90a43bb5c4d4aa67c98555bfebe01c288a21cb23
turbiani <leo.turbiani@gmail.com>
2016-07-15 21:54:28 -0300

    FistCommit

---
+++ app/src/main/java/nanodegree/basic/android/turbiani/com/br/smallbusiness/MainActivity.java
@@ -0,0 +1,13 @@
+package nanodegree.basic.android.turbiani.com.br.smallbusiness;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
